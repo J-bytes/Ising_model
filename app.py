f450d4cfee99206e8200ae5305004cdec04cfb04
J-bytes <60334318+J-bytes@users.noreply.github.com>
2020-03-12 14:22:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -144,12 +144,12 @@ from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = flask.Flask(__name__)
 
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-#app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
 
 
